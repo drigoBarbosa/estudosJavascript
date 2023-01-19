b1db1bc102dad8bc21809b34d27b556a5b6af8dd
Rodrigo Barbosa <rodrigobs8157@gmail.com>
2023-01-18 23:29:09 -0300

    estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO ÍNICIO.
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo -> ${c}`)
+    c++
+}
+
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL.
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+// ESTRUTURA DE REPETIÇÃO COM VARIAVEL DE CONTROLE
+
+for (var c = 1; c <= 10; c++){
+    console.log('cont -> ' + c)
+}
+
+// O que está entra parenteses? 
+
+/* 
+for (inicio; testeLogico; incremento{
+    bloco
+} 
+*/
+
